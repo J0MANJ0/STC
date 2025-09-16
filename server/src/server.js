6fdad9ce45a756d4bf692cedb86ebc6dd38d0903
Joseph Mark <josephmarkmay@gmail.com>
2025-09-16 17:11:55 +0300

    deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,32 @@
-import express, { json } from 'express';
+import express, { json, static as static_ } from 'express';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 import 'dotenv/config';
+
+// routes
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 const app = express();
 
-// middlewares
-app.use(express.json());
+const __dirname = path.resolve();
 
-// routes
+// middlewares
+app.use(json());
+app.use(cookieParser());
+
+// endpoints
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
 const PORT = process.env.PORT;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(static_(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => console.log(`\nSERVER on http://localhost:${PORT}\n`));
